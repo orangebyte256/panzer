eb9d84c061cc2ccd67fd6d5454af5cd05579737f
orangebyte256 <orangebyte@gmail.com>
2016-12-20 21:54:52 +0700

    new commit

--- src/test/java/panzer/AppTest.java
+++ src/test/java/panzer/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest
      */
     public void testApp()
     {
-        System.out.println("Hello test");
+        System.out.println("Hello test new");
         assertTrue( true );
     }
 }
